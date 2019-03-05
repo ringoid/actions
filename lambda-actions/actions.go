b2cd6db1a1a851dd15e99d327550dd10616f8d3c
angryTit <mikhail.tiamine@gmail.com>
2019-03-05 13:44:49 +0300

    add message id

--- lambda-actions/actions.go
+++ lambda-actions/actions.go
@@ -16,6 +16,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/ringoid/commons"
 	"sort"
+	"crypto/md5"
+	"io"
 )
 
 var anlogger *commons.Logger
@@ -173,7 +175,12 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 				anlogger.Errorf(lc, "actions.go :  userId [%s], return %s to client", userId, errStr)
 				return events.APIGatewayProxyResponse{StatusCode: 200, Body: errStr}, nil
 			}
-			event = commons.NewUserMsgEvent(userId, each.TargetPhotoId, originPhotoId, each.TargetUserId, each.SourceFeed, sourceIp, each.Text, each.ActionTime)
+			messageId, ok, errStr := generateMessageId(userId, each.Text, each.ActionTime, lc)
+			if !ok {
+				anlogger.Errorf(lc, "actions.go : userId [%s], return %s to client", userId, errStr)
+				return events.APIGatewayProxyResponse{StatusCode: 200, Body: errStr}, nil
+			}
+			event = commons.NewUserMsgEvent(messageId, userId, each.TargetPhotoId, originPhotoId, each.TargetUserId, each.SourceFeed, sourceIp, each.Text, each.ActionTime)
 			//partitionKey = commons.GeneratePartitionKey(userId, each.TargetUserId)
 			partitionKey = commons.GeneratePartitionKey(userId)
 
@@ -270,6 +277,29 @@ func parseParams(params string, lc *lambdacontext.LambdaContext) (*apimodel.Acti
 
 	anlogger.Debugf(lc, "actions.go : successfully parse request string [%s] to %v", params, req)
 	return &req, true, ""
+}
+
+func generateMessageId(sourceUserId, text string, actionTime int64, lc *lambdacontext.LambdaContext) (string, bool, string) {
+	md := md5.New()
+	_, err := io.WriteString(md, sourceUserId)
+	if err != nil {
+		anlogger.Errorf(lc, "actions.go : error while write source user id string to md5 algo, source user id [%s] : %v", sourceUserId, err)
+		return "", false, commons.InternalServerError
+	}
+	_, err = io.WriteString(md, fmt.Sprintf("%v", actionTime))
+	if err != nil {
+		anlogger.Errorf(lc, "actions.go : error while write action time string to md5 algo, actionTime [%v] : %v", actionTime, err)
+		return "", false, commons.InternalServerError
+	}
+	_, err = io.WriteString(md, text)
+	if err != nil {
+		anlogger.Errorf(lc, "actions.go : error while write text string to md5 algo, text [%s] : %v", text, err)
+		return "", false, commons.InternalServerError
+	}
+
+	resultMessageId := fmt.Sprintf("%x", md.Sum(nil))
+	anlogger.Debugf(lc, "actions.go : successfully generate message id [%s]", resultMessageId)
+	return resultMessageId, true, ""
 }
 
 func main() {
